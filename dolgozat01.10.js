5a47321ce5b2cd0380ea80bcb21d910672e70ee9
Patrick0601 <100844354+Patrik0601@users.noreply.github.com>
2025-01-10 11:18:28 +0100

    Add files via upload

---
+++ dolgozat01.10.js
@@ -0,0 +1,54 @@
+function getGrade (s1, s2, s3) {
+    var score = (s1 + s2 + s3)/3
+    if(score < 60){
+      return 'F'
+    }
+    else if(score < 70){
+      return 'D'
+    }
+    else if(score < 80){
+      return 'C'
+    }
+    else if(score < 90){
+      return 'B'
+    }
+    else if(90 <= score < 100){
+      return 'A'
+    }
+}
+
+function divisibleByThree(str){
+    let sum = 0
+    for (let i = 0; i < str.length; i++){
+      let element = str[i];
+      sum += Number(element);
+    } 
+    if(sum % 3 == 0){
+       return true
+     }
+     return false
+}
+
+function oddOrEven(array) {
+    let sum = 0
+    array.forEach(element => {
+      sum += element
+    });
+   if(sum % 2 === 0){
+     return 'even'
+   }
+   else{
+     return 'odd'
+   }
+}
+
+var isAnagram = function(test, original) {
+    var t = test.toLowerCase().split('').sort().join('');
+    var o = original.toLowerCase().split('').sort().join('');
+    if (t == o){
+      return true
+    }
+    else{
+     return false 
+    }
+};
